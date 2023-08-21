9c7f83788480b1083600606724df0211e57a7834
WilliamC111 <110690003+WilliamC111@users.noreply.github.com>
2023-08-21 17:08:44 -0500

    Add files via upload

---
+++ Sets/src/co.uptc.edu.view/View.java
@@ -0,0 +1,37 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.Vector;
+
+public class View {
+    public Set<Integer> ingresarConjunto() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingresa los elementos del conjunto separados por comas: ");
+        String input = scanner.nextLine();
+        scanner.close();
+
+        Set<Integer> conjunto = new HashSet<>();
+        String[] elementos = input.split(",");
+        for (String elemento : elementos) {
+            conjunto.add(Integer.parseInt(elemento));
+        }
+
+        return conjunto;
+    }
+
+    public void mostrarConjuntoPotencia(Set<Integer> conjuntoEntrada, Set<Set<Integer>> conjuntoPotencia) {
+        System.out.println("Conjunto de entrada: " + conjuntoEntrada);
+        System.out.println("Conjunto Resultado: " + conjuntoPotencia);
+    }
+
+    public void updateArticleList(Vector<String> articleStrings) {
+    }
+
+    public void updateTotalPrice(double totalPrice) {
+    }
+
+    public void setPresenter(Presenter presenter) {
+    }
+   
+}
+   
